1a38336f20340a7331d1f1ae91fe38a2e33726fb
BrandiCook <cookb4@oregonstate.edu>
2022-05-17 19:55:01 -0500

    citation to wsgi

--- wsgi.py
+++ wsgi.py
@@ -1,4 +1,10 @@
 # file for gunicorn
+
+# Citation for wsgi.py:
+# Date: 05/17/2022
+# Based on: OSU CS340 flask-starter-app
+# Source URL: https://github.com/osu-cs340-ecampus/flask-starter-app
+
 from app import app
 
 if __name__ == "__main__":
