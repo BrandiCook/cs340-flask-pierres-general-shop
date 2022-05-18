88a7344d7d7fd33e9abeadd81d6ea4b1948f8d72
BrandiCook <cookb4@oregonstate.edu>
2022-05-17 21:24:53 -0500

    added database and template

--- app.py
+++ app.py
@@ -14,7 +14,33 @@ app = Flask(__name__)
 
 @app.route('/')
 def root():
-    return "Welcome to the OSU CS 340 - Flask Tutorial!"
+    people_from_app_py = [
+        {
+            "name": "Thomas",
+            "age": 33,
+            "location": "New Mexico",
+            "favorite_color": "Blue"
+        },
+        {
+            "name": "Gregory",
+            "age": 41,
+            "location": "Texas",
+            "favorite_color": "Red"
+        },
+        {
+            "name": "Vincent",
+            "age": 27,
+            "location": "Ohio",
+            "favorite_color": "Green"
+        },
+        {
+            "name": "Alexander",
+            "age": 29,
+            "location": "Florida",
+            "favorite_color": "Orange"
+        }
+        ]
+    return render_template("main.j2", people=people_from_app_py)
 
 # Listener
 
