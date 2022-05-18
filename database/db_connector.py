6571f5738e98418e62706a052b937b1f7bd2fc63
BrandiCook <71798408+BrandiCook@users.noreply.github.com>
2022-05-18 16:22:57 -0500

    Update db_connector.py

--- database/db_connector.py
+++ database/db_connector.py
@@ -1,4 +1,4 @@
-# Citation for __init__.py:
+# Citation for db_connector.py:
 # Date: 05/18/2022
 # Based on: OSU cs340_starter_app
 # Source URL: https://github.com/mlapresta/cs340_starter_app and https://github.com/osu-cs340-ecampus/flask-starter-app
