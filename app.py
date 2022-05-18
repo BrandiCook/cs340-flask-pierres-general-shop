114482458cca00d4e00a032b32a9fa7a62189a76
BrandiCook <cookb4@oregonstate.edu>
2022-05-17 19:30:49 -0500

    installations + app.py fiddling

--- app.py
+++ app.py
@@ -2,24 +2,33 @@
 # Date: 05/17/2022
 # Based on: OSU CS340 flask-starter-app
 # Source URL: https://github.com/osu-cs340-ecampus/flask-starter-app
-from flask import Flask
-import os
 
-# Configuration
+from flask import Flask, render_template, json, redirect
+from flask_mysqldb import MySQL
+from flask import request
+import os
 
 app = Flask(__name__)
 
-# Routes 
+app.config['MYSQL_HOST'] = 'classmysql.engr.oregonstate.edu'
+app.config['MYSQL_USER'] = 'cs340_cookb4'
+app.config['MYSQL_PASSWORD'] = '9286' #last 4 of onid
+app.config['MYSQL_DB'] = 'cs340_cookb4'
+app.config['MYSQL_CURSORCLASS'] = "DictCursor"
 
+
+mysql = MySQL(app)
+
+
+# Routes
 @app.route('/')
 def root():
     return "Welcome to the OSU CS 340 - Flask Tutorial!"
 
-# Listener
 
+# Listener
 if __name__ == "__main__":
-    port = int(os.environ.get('PORT', 92866)) 
-    #                                 ^^^^
-    #              You can replace this number with any valid port
-    
-    app.run(port=port) 
+
+    #Start the app on port 9286
+    port = int(os.environ.get('PORT', 9287))
+    app.run(port=port, debug=True)
