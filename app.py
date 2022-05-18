344e0292f6fc44f396a36cefcaf8020691ee9db2
BrandiCook <cookb4@oregonstate.edu>
2022-05-17 23:08:47 -0500

    fixing venv folder

---
+++ app.py
@@ -0,0 +1,27 @@
+from flask import Flask, render_template
+import database.db_connector as db
+import os
+
+# Configuration
+
+app = Flask(__name__)
+db_connection = db.connect_to_database()
+
+# Routes 
+
+@app.route('/')
+def root():
+    return render_template("main.j2")
+
+@app.route('/bsg-people')
+def bsg_people():
+    return "This is the bsg-people route."
+
+# Listener
+
+if __name__ == "__main__":
+    port = int(os.environ.get('PORT', 9112)) 
+    #                                 ^^^^
+    #              You can replace this number with any valid port
+    
+    app.run(port=port, debug=True) 
