20a1efb200867dbf4581e125774af051242bd331
BrandiCook <cookb4@oregonstate.edu>
2022-05-17 21:56:31 -0500

    .gitignore + app and db

--- app.py
+++ app.py
@@ -3,44 +3,41 @@
 # Based on: OSU CS340 flask-starter-app
 # Source URL: https://github.com/osu-cs340-ecampus/flask-starter-app
 
-from flask import Flask, render_template
+from flask import Flask, render_template, json
+import database.db_connector as db
 import os
 
 # Configuration
 
 app = Flask(__name__)
+db_connection = db.connect_to_database() 
 
 # Routes 
 
 @app.route('/')
 def root():
-    people_from_app_py = [
-        {
-            "name": "Thomas",
-            "age": 33,
-            "location": "New Mexico",
-            "favorite_color": "Blue"
-        },
-        {
-            "name": "Gregory",
-            "age": 41,
-            "location": "Texas",
-            "favorite_color": "Red"
-        },
-        {
-            "name": "Vincent",
-            "age": 27,
-            "location": "Ohio",
-            "favorite_color": "Green"
-        },
-        {
-            "name": "Alexander",
-            "age": 29,
-            "location": "Florida",
-            "favorite_color": "Orange"
-        }
-        ]
-    return render_template("main.j2", people=people_from_app_py)
+    return render_template("main.j2")
+
+@app.route('/Employees')
+def employees():
+
+    query = "SELECT * FROM Employees;"
+
+    # The way the interface between MySQL and Flask works is by using an
+    # object called a cursor. Think of it as the object that acts as the
+    # person typing commands directly into the MySQL command line and
+    # reading them back to you when it gets results
+    cursor = db.execute_query(db_connection=db_connection, query=query)
+
+    # The cursor.fetchall() function tells the cursor object to return all
+    # the results from the previously executed
+    #
+    # The json.dumps() function simply converts the dictionary that was
+    # returned by the fetchall() call to JSON so we can display it on the
+    # page.
+    results = json.dumps(cursor.fetchall())
+
+    return results
 
 # Listener
 
